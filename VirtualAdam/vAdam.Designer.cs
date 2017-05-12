75d6c5e9e035d12f5950fb710fb48137f4129c8c
Houdayer Simon <houdayersimon@gmail.com>
2017-05-12 13:39:22 +0200

    Add files via upload

---
+++ VirtualAdam/vAdam.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace VirtualAdam
+{
+    partial class vAdam
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(vAdam));
+            this.tb_log = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tb_addr = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_rep = new System.Windows.Forms.TextBox();
+            this.tb_addr2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tb_log
+            // 
+            this.tb_log.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_log.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.tb_log.Location = new System.Drawing.Point(16, 14);
+            this.tb_log.Margin = new System.Windows.Forms.Padding(4);
+            this.tb_log.Multiline = true;
+            this.tb_log.Name = "tb_log";
+            this.tb_log.Size = new System.Drawing.Size(478, 148);
+            this.tb_log.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(394, 252);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 62);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Flush";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tb_addr
+            // 
+            this.tb_addr.Location = new System.Drawing.Point(231, 252);
+            this.tb_addr.Margin = new System.Windows.Forms.Padding(4);
+            this.tb_addr.Name = "tb_addr";
+            this.tb_addr.Size = new System.Drawing.Size(48, 22);
+            this.tb_addr.TabIndex = 2;
+            this.tb_addr.Text = "17";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 252);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Module 4017 à l\'adresse :";
+            // 
+            // tb_rep
+            // 
+            this.tb_rep.Location = new System.Drawing.Point(20, 206);
+            this.tb_rep.Margin = new System.Windows.Forms.Padding(4);
+            this.tb_rep.Name = "tb_rep";
+            this.tb_rep.Size = new System.Drawing.Size(474, 22);
+            this.tb_rep.TabIndex = 4;
+            // 
+            // tb_addr2
+            // 
+            this.tb_addr2.Location = new System.Drawing.Point(231, 294);
+            this.tb_addr2.Margin = new System.Windows.Forms.Padding(4);
+            this.tb_addr2.Name = "tb_addr2";
+            this.tb_addr2.Size = new System.Drawing.Size(48, 22);
+            this.tb_addr2.TabIndex = 2;
+            this.tb_addr2.Text = "21";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 294);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Module 4050 à l\'adresse :";
+            // 
+            // vAdam
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 336);
+            this.Controls.Add(this.tb_rep);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_addr2);
+            this.Controls.Add(this.tb_addr);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tb_log);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "vAdam";
+            this.Text = "ADAM virtuel";
+            this.Load += new System.EventHandler(this.vAdam_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tb_addr;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.TextBox tb_log;
+        private System.Windows.Forms.TextBox tb_rep;
+        private System.Windows.Forms.TextBox tb_addr2;
+        private System.Windows.Forms.Label label2;
+
+
+    }
+}
+
---
+++ VirtualAdam/vAdam.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+
+namespace VirtualAdam
+{
+    public partial class vAdam : Form
+    {
+        int counter4017 = 0;
+        SerialPort port = new SerialPort("COM2", 9600, Parity.None, 8, StopBits.One);//meme parametre que GTC mais sur com2
+        string rep = " ";//init pour +=
+        string cmdRecues = "";
+        public vAdam()
+        {
+
+            InitializeComponent();
+            tb_log.Text = rep;
+            port.Open();
+            port.DataReceived +=
+            new SerialDataReceivedEventHandler(port_DataReceived);
+
+        }
+        public void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            rep += port.ReadExisting();
+
+            tb_log.BeginInvoke((Action)(() =>
+                       {
+                           tb_log.Text = rep;//log toutes les requetes reçues 
+                       }));
+
+            int addr = Convert.ToInt16(tb_addr.Text);
+            int addr2 = Convert.ToInt16(tb_addr2.Text);
+
+            tb_rep.BeginInvoke((Action)(() =>
+            {
+                
+                tb_rep.Text = cmdRecues;//log toutes les requetes reçues 
+
+                if (rep.Contains("$" + addr.ToString("X2") + "2"))
+                {//si dans lesemble des requete il ya eu celle attendue   si $xx2
+                      string cmdRecue=("$" + addr.ToString("X2") + "2") ;
+                    port.Write("!" + addr.ToString("X2") + "0A0600\r");//repondre en tant que le module repondre !xx0A0600
+                    rep = rep.Replace(cmdRecue, "");
+                    cmdRecues += cmdRecue;
+
+                }
+
+                else if (rep.Contains("$" + addr2.ToString("X2") + "2"))
+                {   string cmdRecue=("$" + addr2.ToString("X2") + "2");
+                    port.Write("!" + addr.ToString("X2") + "070600\r");
+                    rep = rep.Replace(cmdRecue, "");
+                    cmdRecues += cmdRecue;
+                }
+
+                else if (rep.Contains("#" + addr.ToString("X2")) )//4017 demande de lecture
+                {
+                    string[] strLect = {">-00.024-00.024-00.024-00.024-00.023-00.024-00.000-00.022",
+                                        ">-00.024-00.023-00.024-00.023+02.598+00.104-00.021-00.024",
+                                        ">-00.024-00.024-00.024-00.023+02.599+00.104-00.022-00.023",
+                                        ">-00.023-00.024-00.023-00.023+02.599+00.104-00.021-00.023",
+                                        ">-00.023-00.023-00.023-00.023+02.599+00.106-00.022-00.023",
+                                        ">-00.024-00.024-00.024-00.024-00.016-00.022-00.024-00.024",
+                                        ">-00.024-00.024-00.024-00.024-00.017-00.024-00.024-00.024",
+                                        ">-00.024-00.023-00.024+02.628+00.109-00.022+00.000-00.022",
+                                        ">-00.024-00.023-00.023+02.629+00.110-00.021-00.023-00.023",
+                                        ">-00.023-00.023-00.023-00.021-00.023-00.023-00.023-00.022",
+                                        ">-00.023-00.023-00.024-00.022-00.024-00.024-00.024-00.024",
+                                        ">-00.022-00.022-00.022+02.627+00.109-00.021-00.022-00.022",
+                                        ">-00.022-00.022-00.023+02.627+00.107-00.020-00.022-00.022",
+                                        ">-00.023-00.023-00.024+02.768+00.117-00.020-00.023-00.023",
+                                        ">-00.024-00.023-00.024+02.870+00.120-00.021-00.023-00.024",
+                                        ">-0.0234-0.0233-0.0235+999999+0.1077-0.0224-0.0235-0.0232",
+                                        ">-0.0339-0.0241-0.0241+999999+0.1013-0.0222-0.0240-0.0241",
+                                        };
+
+
+
+                    string cmdRecue = ("#" + addr.ToString("X2"));
+                    port.Write(strLect[counter4017] + "\r");
+                    if (counter4017 < strLect.Length-1) {
+                        counter4017++;
+                    }else if (counter4017==strLect.Length-1){
+                        counter4017 = 0;
+                    }
+
+                    //ihm adam virtuel
+                    rep = rep.Replace(cmdRecue, "");
+                    cmdRecues += cmdRecue;
+
+                }
+
+                else if(rep.Contains("$"+addr2.ToString("X2") + '6') ){//4050
+                    string cmdRecue = ("$" + addr2.ToString("X2")+'6');
+                    port.Write(">04\r");
+                    rep = rep.Replace(cmdRecue, "");
+                    cmdRecues += cmdRecue;
+                }
+
+                else{
+                    if (rep.Length > 3000) {
+                        rep = "";
+                    }
+                }
+
+
+            }));
+
+        }
+
+
+
+        ~vAdam()
+        {
+            port.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rep = " ";
+            tb_rep.Text = " ";
+
+
+        }
+
+        private void vAdam_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
